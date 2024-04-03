8cc9002f99afa5422894afdf0984d752a102b70c
byulcode <bybit0408@gmail.com>
2024-04-03 13:08:55 +0900

    refactor: 질문 키워드 검색 성능 개선 (#161)
    * refactor : findQuestionsByKeywordAndCategoryNamesWithPage 메서드 인자 수정
    
    * feat: MySQL full text index 방언 등록
    
    * feat: 질문 조회시 like -> match_against 쿼리로 변경

---
+++ src/main/java/com/i6/honterview/common/contributor/MatchFunctionContributor.java
@@ -0,0 +1,17 @@
+package com.i6.honterview.common.contributor;
+
+import org.hibernate.boot.model.FunctionContributions;
+import org.hibernate.boot.model.FunctionContributor;
+import org.hibernate.type.StandardBasicTypes;
+
+public class MatchFunctionContributor implements FunctionContributor {
+	private static final String FUNCTION_NAME = "match_against";
+	private static final String FUNCTION_PATTERN = "match(?1) against (?2 in boolean mode)";
+
+	@Override
+	public void contributeFunctions(final FunctionContributions functionContributions) {
+		functionContributions.getFunctionRegistry()
+			.registerPattern(FUNCTION_NAME, FUNCTION_PATTERN,
+				functionContributions.getTypeConfiguration().getBasicTypeRegistry().resolve(StandardBasicTypes.DOUBLE));
+	}
+}
