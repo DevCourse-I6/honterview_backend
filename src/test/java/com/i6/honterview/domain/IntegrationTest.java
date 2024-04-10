bd180d1150a5378a91c7c58081e003ef5f5763ba
ebcho <joeunbi514@naver.com>
2024-04-11 00:24:44 +0900

    test: testContainers를 활용한 통합테스트 작성중

--- src/test/java/com/i6/honterview/domain/IntegrationTest.java
+++ src/test/java/com/i6/honterview/domain/IntegrationTest.java
@@ -4,8 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -13,12 +20,32 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc
 @ActiveProfiles(profiles = "test")
 @Transactional
-public class IntegrationTest {
+@Testcontainers
+public abstract class IntegrationTest {
 
 	@Autowired
-	protected MockMvc mvc;
+	protected MockMvc mockMvc;
 
 	@Autowired
 	protected ObjectMapper objectMapper;
+
+	@Container
+	public static GenericContainer<?> redis = new GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
+		.withExposedPorts(6379);
+
+	@Container
+	public static MySQLContainer<?> mysql = new MySQLContainer<>(DockerImageName.parse("mysql:5.7"))
+		.withDatabaseName("testdb")
+		.withUsername("test")
+		.withPassword("test");
+
+	@DynamicPropertySource
+	static void redisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.redis.host", redis::getHost);
+		registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+		registry.add("spring.datasource.url", mysql::getJdbcUrl);
+		registry.add("spring.datasource.username", mysql::getUsername);
+		registry.add("spring.datasource.password", mysql::getPassword);
+	}
 
 }
